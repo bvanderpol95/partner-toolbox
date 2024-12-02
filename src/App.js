2b3de502218d1b9eaefaab1a79e329ce0646dd21
Bas van der Pol <bvanderpol@usetappr.com>
2024-12-02 09:38:05 +0100

    Put calculator on different sbudomain

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import EnterpriseQuoteBuilder from './EnterpriseQuoteBuilder';
 import './index.css';
 
 const App = () => {
   return (
-    <BrowserRouter basename="/enterprise/quote-builder">
+    <Router>
       <div className="bg-gray-900 text-white min-h-screen">
-        <EnterpriseQuoteBuilder />
+        <main className="p-4">
+          <Routes>
+            <Route path="/enterprise/quote-builder" element={<EnterpriseQuoteBuilder />} />
+
+            {/* Add more routes as needed */}
+          </Routes>
+        </main>
       </div>
-    </BrowserRouter>
+    </Router>
   );
 };
 
